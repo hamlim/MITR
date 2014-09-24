5c471db9d8ac9f7b89e025c2e3fd66c1ba9aca16
Hydriad <schluh@rpi.edu>
2014-09-24 17:38:08 -0400

    cards and columns in app

--- header.php
+++ header.php
@@ -1,29 +1,21 @@
 <!DOCTYPE html>
 <html class="no-js">
     <head>
-        <script src="http://code.jquery.com/jquery-latest.min.js"
-        type="text/javascript"></script>
+        <!--link to latest jquery-->
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Application</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-
+        <!--links to stylesheets-->
         <link rel="stylesheet" href="uikit-2.10.0/css/uikit.css" />
         <link rel="stylesheet" href="styles/custom.css">
+        <!--link to js-->
         <script src="uikit-2.10.0/js/uikit.min.js"></script>
-
-
     </head>
     <body>
-    <!--
-        <div id = "header" class = >
-            <a href = "app.php"><img src = "styles/pics/C-Quip_header.png"></a>
-            <a href = "#">Recent Activity</a>
-            <a href = "#">AcctName</a>
-        </div>
-    -->
         <nav class="uk-navbar" id = "navyo">
             <a href = "app.php"><img src = "styles/pics/C-Quip_header.png"></a>
             <div class="uk-navbar-flip">
