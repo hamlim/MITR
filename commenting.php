1258eea5abe4fe0acc5633b5cf50169beeac39b4
piyaws <piyaws@rpi.edu>
2014-09-24 15:16:46 -0400

    Update commenting.php
    These are the codes that add commentName, commentUsername and commentContent to the Comment table. We need codes to submit the comments and display on the page.

--- commenting.php
+++ commenting.php
@@ -1,4 +1,18 @@
 <?php
-
-
+  if ($_POST) {
+    $commentName = $_POST['name'];
+    $commentname = mysql_real_escape_string($commentName);
+    $commentUsername = $_POST['username'];
+    $commentUsername = mysql_real_escape_string($commentUsername);
+    $commentContent = $_POST['content'];
+    $commentContent = mysql_real_escape_string($commentContent);
+    $cardId = $_POST['cardid']; 
+    $cardId = mysql_real_escape_string($cardId);
+    mysql_query("insert into comments(commentName,commentUsername,commentContent) values ('$commentName','$commentUsername','$commentContent','$cardId')");
+  }
 ?>
+
+<li class="box">
+<?php echo $commentName;?><br />
+<?php echo $commentContent; ?>
+</li>
