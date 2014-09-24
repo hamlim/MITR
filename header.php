26f50167cbe8b1bf26c572d1d7e92258c43d48b0
Hydriad <schluh@rpi.edu>
2014-09-24 12:17:55 -0400

    header.php
    yo

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Application</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/custom.css">
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+        <link rel="stylesheet" href="uikit-2.10.0/css/uikit.min.css" />
+        <script src="uikit-2.10.0.js/uikit.min.js"></script>
+
+
+    </head>
+    <body>
+        <div id = "header">
+            <a href = "app.php"><img src = "styles/pics/C-Quip_header.png"></a>
+            <a href = "#">Recent Activity</a>
+            <a href = "#">AcctName</a>
+        </div>
+
+    </body>
+</html>
+
