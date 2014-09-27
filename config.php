7838b6cd1f4a2a585bb8b8a91b7d0fb5dc28bc96
hamlim <matt@Neil-Hamlins-MacBook-Pro-3.local>
2014-09-27 15:08:45 -0400

    some notes on cofig.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 	//a simple file to handle the database information and keep it seperate from the rest of the stuff
 
 	$config = array(
-		'host'       => '',
+		'host'       => '', //change this on your own system
 		'db_name'    => '',
-		'db_username'=> '',
-		'db_password'=> '',
+		'db_username'=> '', //change this on your own system
+		'db_password'=> '', //change this on your own system as well
 		'db_version' => '',
 		'debug'      => '',
 	);
