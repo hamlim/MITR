cd66e825776bc03c7f6e4179f29a0a1f8be7c949
cmrenus <cmrenus@gmail.com>
2014-10-26 18:30:15 -0400

    Start of settings page

--- settings.php
+++ settings.php
@@ -1,66 +1,84 @@
+<?php
+    //started of with some html5boilerplate from html5boilerplate.com
+    include "header.php";
+?>
+
+
 <!DOCTYPE html>
-<html class="no-js">
+<html class="no-js" style="height:100%;">
     <head>
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Settings</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="uikit-2.10.0/css/uikit.css" />
+        <link rel="stylesheet" href="styles/custom.css" />
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <script src="js/custom.js" type="text/javascript"></script>
+        <script src="uikit-2.10.0/js/uikit.min.js" type="text/javascript"></script>
     </head>
-    <body>
+    <body style="height: 100%;">
+        <div class ="settingsContent">
+            <div class = "settingsName">
+                <img src="http://www.ihd-wallpapers.com/wp-content/uploads/2014/08/Grumpy_cat_wallpapers.jpg" style = "width:100px;height:100px" />
+                <div>Christopher Renus</div>
+            </div>
+            <!--<ul class="uk-tab uk-tab-grid uk-tab-bottom" data-uk-tab>
+                <li class="uk-width-1-3"><a href="">...</a></li>
+            </ul>-->
+            <div>
+                <form class="uk-form-horizontal" >
+                    <fieldset class="settings">
+                        <h1>Account settings</h1>
+                        <div class="uk-form-row">
+                            <label class="uk-form-label" for="">Username:</label>
+                            <input type="text" placeholder="Username">
+                            <span class="uk-form-help-inline"></span>
+                        </div>
+                        <div class="uk-form-row">
+                            <label class="uk-form-label" for="">Password:</label>
+                            <input type="text" placeholder="Current">
+                        </div>
+                        <div class="uk-form-row">
+                            <label class="uk-form-label" for=""></label>
+                            <input type="text" placeholder="New Password">
+                        </div>
+                        <div class="uk-form-row">
+                            <label class="uk-form-label" for=""></label>
+                            <input type="text" placeholder="Confirm Password">
+                        </div>
+                        <div class="uk-form-row">
+                            <label class="uk-form-label" for="">Color Scheme:</label>
+                            <div class="uk-button-dropdown" data-uk-dropdown>
 
-        <div class="header-container">
-            <header class="wrapper clearfix">
-                <h1 class="title">h1.title</h1>
-                <nav>
-                    <ul>
-                        <li><a href="#">nav ul li a</a></li>
-                        <li><a href="#">nav ul li a</a></li>
-                        <li><a href="#">nav ul li a</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+                                <button class="uk-button">...</button>
 
-        <div class="main-container">
-            <div class="main wrapper clearfix">
+                                <!-- This is the dropdown -->
+                                <div class="uk-dropdown uk-dropdown-small">
+                                    <ul class="uk-nav uk-nav-dropdown">
+                                        <li><a href="#" id="color1"></a></li>
+                                        <li><a href="#" id="color2"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="uk-button uk-button-large button">Save Changes</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
 
-                <article>
-                    <header>
-                        <h1>article header h1</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
-                    </header>
-                    <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-                    </section>
-                    <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-                    </section>
-                    <footer>
-                        <h3>article footer h3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
-                    </footer>
-                </article>
-
-                <aside>
-                    <h3>aside</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
-                </aside>
-
-            </div> <!-- #main -->
-        </div> <!-- #main-container -->
-
-        <div class="footer-container">
-            <footer class="wrapper">
-                <h3>footer</h3>
-            </footer>
+            <div class="footer-container">
+                <footer class="wrapper">
+                    <h3></h3>
+                </footer>
+            </div>
         </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
