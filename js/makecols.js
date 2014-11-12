9107c7a1f2be7fe3d94a8eb54b4f19fca4847163
hamlim <matt@graben-244.dynamic2.rpi.edu>
2014-11-12 13:59:51 -0500

    changes

--- js/makecols.js
+++ js/makecols.js
@@ -1,11 +1,17 @@
 //columndata = JSON of columns
 
+//first sort the columns by their order
+var colu = columndata.sort(function(a,b){return (a.columnorder - b.columnorder);});
+//console.log for testing
+console.log(colu);
+//make an array of column names
 var columnnames = [];
 for(i=0; i<columndata.length; i++){
     columnnames.push(columndata[i]["columnname"]);
 }
+//testing
 console.log(columnnames);
-
+//grab the column body element (holds all columns in it)
 var colbody = document.getElementById("columnslist");
 
 //parlor trick code, not actually meant to work
@@ -19,7 +25,14 @@ for(i=0; i<columnnames.length; i++){
 
 //now we make the html content from the column lists
 //we also need the cards
+//colu is the array of order-sorted columns
 //
-//for(i=0; i<columndata.length; i++){
-//    for(j=0; j<carddata.length; j++){
-//        if(carddata[j]["info"]["columnID"] == 
+for(i=0; i<colu.length; i++){ //iterate through columns
+    for(j=0; j<carddata.length; j++){ //iterate through cards
+        if(carddata[j]["info"]["columnID"] == colu[i]["columnID"]){
+            //the card belongs in the column
+            //we need to format the card properly
+            var name = carddata[j]["info"]["name"];
+            var priority = carddata[j]["info"]["priority"];
+            var color = carddata[j]["info"]["colorcode"];
+            
