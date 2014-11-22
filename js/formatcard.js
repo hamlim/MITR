147d864a30ef51f168de988a9e14bf4aac2f6314
hamlim <matt@Neil-Hamlins-MacBook-Pro-3.local>
2014-11-22 17:19:16 -0500

    added JS file

---
+++ js/formatcard.js
@@ -0,0 +1,10 @@
+//format card.js
+function popupformatcard() {
+    //here we handle the vx popup modal
+    //the data that the user then outputs
+    //creation of the card
+    //adding the card to the local storage
+    //then adding the card to the server cards.json file
+    
+    
+}
