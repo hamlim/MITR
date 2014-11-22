bd13e9a7a0c7e29fd95be8841f9b3c3d9d2a0ab8
Hydriad <schluh@rpi.edu>
2014-11-22 15:10:14 -0500

    editcard classes

--- js/editcard.js
+++ js/editcard.js
@@ -94,13 +94,13 @@ function popupeditcard(cardID){
     //---------------------------------------------
     //title
     
-    var ftitle = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary'><label for='title'>Card Title:</label><div class='vex-custom-input-wrapper'><input name='title' type='text' value='" + n + "'/></div></div></div><br/>";
+    var ftitle = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary customizetitle'><label for='title'>Card Title:</label><div class='vex-custom-input-wrapper'><input name='title' type='text' value='" + n + "'/></div></div></div><br/>";
     //---------------------------------------------
     //priority
-    var fpri = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary'><label for='priority'>Card Priority:</label><div class='vex-custom-input-wrapper'><input name='priority' type='number' min=1 value='"+p+"'/></div></div></div><br/>";
+    var fpri = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary customizepriority'><label for='priority'>Card Priority:</label><div class='vex-custom-input-wrapper'><input name='priority' type='number' min=1 value='"+p+"'/></div></div></div><br/>";
     //---------------------------------------------
     //color codes
-    var fcolor = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary'><label for='colorcode'>Card Colorcode:</label>";
+    var fcolor = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary customizedropdown'><label for='colorcode'>Card Colorcode:</label>";
     var optionstring = "<div class='vex-custom-input-wrapper'><select name='colorcode' id='card-color-select'><option>"+ccs.name+"</option>";
     for(j=0; j<ncccs.length; j++){
         optionstring += "<option>"+ncccs[j].name+"</option>";
@@ -109,7 +109,7 @@ function popupeditcard(cardID){
     fcolor += optionstring + "</div></div></div><br/>";
     //---------------------------------------------
     //columnID
-    var fcolid = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary'><label for='columnid'>Card Column:</label>";
+    var fcolid = "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary customizedropdown'><label for='columnid'>Card Column:</label>";
     var coloptstring = "<div class='vex-custom-input-wrapper'><select name='columnid' id='column-name-select'><option>"+currentcolname+"</option>";
     for(q=0; q<colnames.length; q++){
         coloptstring += "<option>" + colnames[q] + "</option>";
@@ -119,14 +119,14 @@ function popupeditcard(cardID){
     //all stfs
     var fstf = "<div class='stf-edit-content'>";
     for(y=0; y<cad["stf-fields"].length; y++){
-        fstf += "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary'><label for='stf'>"+cad["stf-fields"][y].fieldname+":</label><div class='vex-custom-input-wrapper'><input name='stf' type='text' value='"+cad["stf-fields"][y].fielddata+"'/></div></div></div><br/>";   
+        fstf += "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary customizestf'><label for='stf'>"+cad["stf-fields"][y].fieldname+":</label><div class='vex-custom-input-wrapper'><input name='stf' type='text' value='"+cad["stf-fields"][y].fielddata+"'/></div></div></div><br/>";   
     }
     fstf += "</div><br/>";
     //---------------------------------------------
     //all ltfs
     var fltf = "<div class='ltf-edit-content'>";
     for(y=0; y<cad["ltf-fields"].length; y++){
-        fltf += "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary'><label for='ltf'>"+cad["ltf-fields"][y].fieldname+":</label><div class='vex-custom-field-wrapper'><textarea name='ltf' rows='4' placeholder='"+cad["ltf-fields"][y].fielddata +"' value='"+cad["ltf-fields"][y].fielddata+"'/></textarea></div></div></div><br/>";   
+        fltf += "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary customizeltf'><label for='ltf'>"+cad["ltf-fields"][y].fieldname+":</label><div class='vex-custom-field-wrapper'><textarea name='ltf' rows='4' placeholder='"+cad["ltf-fields"][y].fielddata +"' value='"+cad["ltf-fields"][y].fielddata+"'/></textarea></div></div></div><br/>";   
     }
     fltf += "</div><br/>";
     //---------------------------------------------
@@ -136,7 +136,7 @@ function popupeditcard(cardID){
         var now = moment(cad["date-fields"][y].fielddata);
         var datet = moment(now).format("YYYY MM DD");
         //attempts at fixing the date format so jQuery understands it
-        fdate += "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary'><label for='dates'>"+cad["date-fields"][y].fieldname+":</label><div class='vex-custom-input-wrapper'><input name='date' type='date' value='"+datet+"'/></div></div></div><br/>";   
+        fdate += "<div class='vex-custom-field-wrapper'><div class='uk-form-row uk-panel-primary customizedate'><label for='dates'>"+cad["date-fields"][y].fieldname+":</label><div class='vex-custom-input-wrapper'><input name='date' type='date' value='"+datet+"'/></div></div></div><br/>";   
     }
     fdate += "</div><br/>";
     // now compile all parts
